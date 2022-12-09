b7c5a941d2b7590b5c4d3c4d745a99ee3093629a
ooishikoon <skooi0307@hotmail.com>
2022-12-09 18:27:20 +0800

    update

--- LabExercise_PHP_Database/list.php
+++ LabExercise_PHP_Database/list.php
@@ -12,9 +12,21 @@ $conn = new mysqli($servername, $username, $dbpassword, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+?>
 
-echo "<h2 style='text-align: center;'><b>Student Data Info List</b></h2>";
+<h2 style='text-align: center;'><b>Student Data Info List</b></h2>
 
+<table class='table table-hover mx-auto w-auto' style='margin-left: auto;margin-right: auto;'>
+<form method=POST form action = "update.php">
+<tr><th>Student</th>
+        <th>Matric</th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Race</th>
+        <th>Gender</th>
+        <th>Actions</th></tr>
+
+<?php
  // List Data
  $sqlstudent = "SELECT * FROM tbl_student";
  $result = $conn->query($sqlstudent);
@@ -22,16 +34,6 @@ echo "<h2 style='text-align: center;'><b>Student Data Info List</b></h2>";
      echo '<script>document.getElementById("reset").style.display = "block"</script>';
  else
      echo '<script>document.getElementById("reset").style.display = "none"</script>';
- 
- echo "<table class='table table-hover mx-auto w-auto' style='margin-left: auto;margin-right: auto;'>
- <form method=POST>";
- echo "<tr><th>Student</th>
-         <th>Matric</th>
-         <th>Name</th>
-         <th>Email</th>
-         <th>Race</th>
-         <th>Gender</th>
-         <th>Actions</th></tr>";
 
  while ($row = mysqli_fetch_assoc($result)) {
      echo "<tr><td>";
@@ -45,8 +47,7 @@ echo "<h2 style='text-align: center;'><b>Student Data Info List</b></h2>";
          <td>" . $row["email"] . "</td>
          <td>" . $row["race"] . "</td>
          <td>" . $row["gender"] . "</td>
-         <td><button type='submit' class='btn btn-outline-danger btn-sm' name='remove' value='" . $row["id"] . "'>Remove</button>
-         <button type='submit' class='btn btn-outline-primary btn-sm' name='edit' value='" . $row["id"] . "'>Edit</button></td></tr>";
+         <td><button type='submit' class='btn btn-outline-primary btn-sm' name='edit' value='" . $row["id"] . "'>Edit</button></td></tr>";
         }
  echo "</table></form>";
 ?>
@@ -67,7 +68,7 @@ echo "<h2 style='text-align: center;'><b>Student Data Info List</b></h2>";
         <div style='text-align:right'>
         <tr>
         Search: <input type="text" name="search">
-        Column: <select name="column">
+        Column: <select name="option" id="option">
             <option value="race">Race</option>
             <option value="gender">Gender</option>
             </select>
@@ -78,8 +79,10 @@ echo "<h2 style='text-align: center;'><b>Student Data Info List</b></h2>";
         <?php
         if(isset($_POST["submit"])){
             $search = $_POST['search'];
-            $sqlstudent  = "SELECT * FROM tbl_student WHERE race = '$search'"; 
-            $sqlstudent  = "SELECT * FROM tbl_student WHERE gender = '$search'"; 
+            $option = $_POST["option"];
+
+            $sqlstudent  = "SELECT * FROM tbl_student WHERE $option = '$search'"; 
+            // $sqlstudent  = "SELECT * FROM tbl_student WHERE gender = '$search'"; 
             $result = $conn->query($sqlstudent);
             echo "<table class='table table-hover mx-auto w-auto' style='margin-left: auto;margin-right: auto;'>
             <form method=POST><h2 style='text-align: center;'>Result</h2>";
@@ -92,6 +95,11 @@ echo "<h2 style='text-align: center;'><b>Student Data Info List</b></h2>";
             if ($result->num_rows > 0){
             while($row = $result->fetch_assoc() ){
                 echo "<tr><tr>
+                <td><img src='data:image/jpg;base64,".base64_encode( $row['std_image'] )."'
+                style='object-fit:contain;
+                    width:100px;
+                    height:100px;
+                    border: solid 1px #CCC'/></td>
                 <td>" . $row["matric"] . "</td>
                 <td>" . $row["name"] . "</td>
                 <td>" . $row["email"] . "</td>
@@ -105,7 +113,6 @@ echo "<h2 style='text-align: center;'><b>Student Data Info List</b></h2>";
             "</table>";
         }
         ?>
-        <!-- echo "<tr><tr><td><button type='submit' class='btn btn-primary'>Save</button></td> -->
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
--- LabExercise_PHP_Database/update.php
+++ LabExercise_PHP_Database/update.php
@@ -11,70 +11,50 @@ $conn = new mysqli($servername, $username, $dbpassword, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} else
-    echo "<center>Successful connected</center>";
-
- // List Data
- $sqlstudent = "SELECT * FROM tbl_student";
- $result = $conn->query($sqlstudent);
- if ($result -> num_rows == 0)
-     echo '<script>document.getElementById("reset").style.display = "block"</script>';
- else
-     echo '<script>document.getElementById("reset").style.display = "none"</script>';
- 
- echo "<center>No of records:" . $result->num_rows . "</center>
- <table class='table table-hover mx-auto w-auto' style='margin-left: auto;margin-right: auto;'>
- <form method=POST>";
- echo "<tr><th>Student</th>
-         <th>Matric</th>
-         <th>Name</th>
-         <th>Email</th>
-         <th>Race</th>
-         <th>Gender</th>
-         <th>Actions</th></tr>";
-
- while ($row = mysqli_fetch_assoc($result)) {
-     echo "<tr><td><button type='submit' class='btn btn-primary'>Save</button></td><td>" . $row["matric"] . "</td><td>" . $row["name"] . "</td><td>" . $row["email"] . "</td><td>" . $row["race"] . "</td><td>" . $row["gender"] . "</td>
-         <td><button type='submit' class='btn btn-outline-danger btn-sm' name='remove' value='" . $row["id"] . "'>Remove</button>
-         <button type='submit' class='btn btn-outline-primary btn-sm' name='edit' value='" . $row["id"] . "'>Edit</button></td></tr>";
-        }
- echo "</table></form>";
-
-// Remove Data Row
-if (isset($_POST["remove"])) {
-    $sql = "DELETE FROM tbl_student WHERE id=" . $_POST['remove'];
-    $conn->query($sql);
-    echo "<center>Data (" . $_POST['remove'] . ") deleted</center>";
 }
 
 // Edit Record
 if (isset($_POST["edit"])) {
-    $sqlstudent = "SELECT * FROM tbl_student WHERE id=" . $_POST["edit"];
+    $sqlstudent = "SELECT * FROM tbl_student WHERE id=" .$_POST["edit"];
     $result = $conn->query($sqlstudent);
+  
     while ($row = mysqli_fetch_assoc($result)) {
-        echo "<center>Edit id=" . $_POST["edit"] . " <form method='POST'><input type='text' class='form-control' name='matric' value='" . $row['matric'] . "' required>";
-        echo "<input type='text' name='name' class='form-control' value='" . $row['name'] . "'>";
-		echo "<input type='text' name='email' class='form-control' value='" . $row['email'] . "'>";
-		echo "<input type='text' name='race' class='form-control' value='" . $row['race'] . "'>";
-		echo "<input type='text' name='gender' class='form-control' value='" . $row['gender'] . "'>";
-        echo "<button type='submit' name='process_edit' value='" . $_POST["edit"] . "' >Submit</button><button><a href=' '></a>Cancel</button></form></center>";
+        echo "<center><form method = 'POST' form action = 'update.php' enctype='multipart/form-data'>
+        <h1>Edit Data</h1>
+        <table class= 'table table-borderless mx-auto w-auto' style='margin-left: auto;margin-right: auto;'>
+        <tr><td><strong>Matric  :</strong></td><td> <input type='text' name='matric' value='" . $row['matric'] . "'><br><br></td></tr>";
+        echo "<tr><td><strong>Name  :</strong></td><td> <input type='text' name='name' value='" . $row['name'] . "'><br><br></td></tr>";
+        echo "<tr><td><strong>Email  :</strong></td><td> <input type='email' name='email' value='" . $row['email'] . "'><br><br></td></tr>";
+        echo "<tr><td><strong>Race  :</strong></td><td> <input type='text' name='race' value='" . $row['race'] . "'><br><br></td></tr>";
+        echo "<tr><td><strong>Gender  :</strong></td><td> <input type='text' name='gender' value='" . $row['gender'] . "'><br><br></td></tr>";
+        echo "<tr><td><strong>Image  :</strong></td><td> <input type='file' name='image' required><br><br></td></tr>";
+        echo "<tr><td colspan='2', style='text-align: center'><button type='submit' class='btn btn-primary' name='process_edit' value='" . $_POST["edit"] . "' >Submit</button></td></tr>
+        </table></form></center>";
+        
     }
-}
+    
+  }
 
 // Update Data
 if (isset($_POST["process_edit"])) {
+
     $id = $_POST["process_edit"];
     $matric = $_POST['matric'];
     $name = $_POST['name'];
-	$email = $_POST['email'];
-	$race = $_POST['race'];
-	$gender = $_POST['gender'];
+    $email = $_POST['email'];
+    $race = $_POST['race'];
+    $gender = $_POST['gender'];
+    //Get the content of the image and then add slashes to it 
+    $imagetmp=addslashes (file_get_contents($_FILES['image']['tmp_name']));
 
-    $sqlupdate = "UPDATE tbl_student SET matric ='$matric', name ='$name', email ='$email', race ='$race', gender ='$gender' WHERE id='$id' ";
+    $sqlupdate = "UPDATE tbl_student set matric ='$matric', `name` ='$name', email ='$email', `race` ='$race', gender ='$gender', `std_image` = '$imagetmp' where Matric='$matric' ";
+
 
     if ($conn->query($sqlupdate) === TRUE) {
-        echo "<center>Data Updated</center>";
+        echo "<center><h2>Data Updated</h2>";
+        echo "<br><a href='list.php'><button type='submit' class='btn btn-success btn-lg' name='back' value=''>Back to List</button></a></center>";
     }
+
 }
 
 // Close Connection
@@ -85,13 +65,14 @@ $conn->close();
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Student</title>
+	<title>User Table UI Design</title>
 	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
+	
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
